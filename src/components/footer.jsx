822fac2a6cb986b35d19a6a1992be4970699b75b
siddhant1405 <76835089+siddhant1405@users.noreply.github.com>
2025-07-05 00:05:03 +0530

    Finalize merge: restructure project and clean up files

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="w-full py-4 bg-gray-50 text-center text-black-800 text-sm mt-12 border-t">
-      &copy; {new Date().getFullYear()} MyResume. All rights reserved.
-    </footer>
-  );
-}
